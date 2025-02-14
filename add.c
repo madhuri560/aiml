654fea6c281d7cd226a132a99a826cb92ac008f7
VAISHNAVI H B <vaishnavi.b@s.amity.edu>
2025-02-14 12:27:40 +0530

    All operations done

--- add.c
+++ add.c
@@ -1,3 +1,6 @@
 a=10
 b=5
 printf("%d",a,b);
+printf("sum is%d",a+b);
+printf("diff is%d",a,b);
+printf("mul is%d",a,b);
---
+++ maths.c
@@ -0,0 +1,5 @@
+a=10
+b=5
+printf("the addition is%d",a+b);
+printf("the substration is%d",a-b);
+printf("the multiplication is%d"a*b);
