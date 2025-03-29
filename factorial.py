24d854ab4aceae4cbcb5a6f36fccee763e249255
VAISHNAVI H B <vaishnavi.b@s.amity.edu>
2025-03-29 20:14:26 +0530

    added new line

--- factorial.py
+++ factorial.py
@@ -3,3 +3,7 @@ fact=1
 for i in range(1,n+1):
     fact*=i
     print(fact)
+
+
+Factorial number
+The factorial of a non-negative integer ( n ), denoted as ( n! ), is the product of all positive integers less than or equal to ( n ).
