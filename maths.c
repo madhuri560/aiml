08485db59efed2f78d5b7849fa530678235504a2
VAISHNAVI H B <vaishnavi.b@s.amity.edu>
2025-03-29 20:24:55 +0530

    added new line of code

--- maths.c
+++ maths.c
@@ -2,4 +2,6 @@ a=10
 b=5
 printf("the addition is%d",a+b);
 printf("the substration is%d",a-b);
-printf("the multiplication is%d"a*b);
+printf("the multiplication is%d",a*b);
+printf("the modulus is%d",a%b);
+
