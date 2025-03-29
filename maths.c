1d8164e348902d1b29848f26a70f16ad1557cdd9
VAISHNAVI H B <vaishnavi.b@s.amity.edu>
2025-03-29 20:30:32 +0530

    added new line of code

--- maths.c
+++ maths.c
@@ -1,9 +1,15 @@
-a=10
-b=5
+#include<stdio.h>
+int main()
+{
+  int a=10
+int b=5
 printf("the addition is%d",a+b);
 printf("the substration is%d",a-b);
 printf("the multiplication is%d",a*b);
 printf("the modulus is%d",a%b);
 printf("the division is%d",a/b);
-printf("the floor division is%d",a//b);
+printf("the floor division is%d",a\\b);
+}
+
+
 
