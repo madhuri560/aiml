902b4976f837ebbb564413058703816cc4f092e0
VAISHNAVI H B <vaishnavi.b@s.amity.edu>
2025-03-29 21:22:58 +0530

    added neon no

--- neon.c
+++ neon.c
@@ -5,4 +5,9 @@ int Check_Neon_Number(int num)
     int square = num * num;  
     int n = square; 
     int digit; 
-    int sum = 0; 
+    int sum = 0;
+     while (n != 0) {
+        digit = n % 10;
+        sum = sum + digit;
+        n = n / 10;
+    }
