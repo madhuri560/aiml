9c3e4dc71efa3cdebf261c28cf4e3d00e4dc7106
VAISHNAVI H B <vaishnavi.b@s.amity.edu>
2025-03-29 21:15:11 +0530

    added astrong no

--- armstrong.c
+++ armstrong.c
@@ -1,16 +1,28 @@
+#include <math.h>
 #include <stdio.h>
-{
-int power(int x, unsigned int y)
-{
-    if (y == 0)
-        return 1;
-     if (y % 2 == 0)
-        return power(x, y / 2) * power(x, y / 2);
-    return x * power(x, y / 2) * power(x, y / 2);
+#include <stdbool.h>
+bool isArmstrong(int N) {
+    int temp = N;
+    int sum=0;
+    int K = log10(temp) + 1;
+    while (temp > 0) {
+        int digit = temp % 10;
+        sum += pow(digit, K);
+        temp /= 10;
+    }
+    return (sum == N);
 }
-int order(int x)
-{
-    int n = 0;
-    while (x)
-    {
-        n++;
+
+int main() {
+    int N = 153;
+    if (isArmstrong(N)) {
+        printf("Yes\n");
+    }
+    else {
+        printf("No\n");
+    }
+
+    return 0;
+}
+
+
