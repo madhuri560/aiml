b4deca0924ea88611768e5f18049b74b8180e6b2
VAISHNAVI H B <vaishnavi.b@s.amity.edu>
2025-03-29 20:36:11 +0530

    added legendres eqn

--- legendres.c
+++ legendres.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-int largestPower(int n, int p) {
+{
+	int largestPower(int n, int p) {
   
     if (n == 0) 
         return 0;
@@ -9,5 +10,6 @@ int main() {
     int n = 10, p = 3;
      printf("%d", largestPower(n, p));
     return 0;
+}
 }
 
