add5f2dc1ed2b4eb14bdcd3609408215f43ef615
VAISHNAVI H B <vaishnavi.b@s.amity.edu>
2025-04-05 20:48:24 +0530

    weekdays

--- weekdys.c
+++ weekdys.c
@@ -4,4 +4,10 @@ int main() {
     int day;
     printf("Enter a number (1-7) to get the corresponding weekday: ");
     scanf("%d", &day);
-
+switch(day) {
+        case 1:
+            printf("Monday\n");
+            break;
+        case 2:
+            printf("Tuesday\n");
+            break;
