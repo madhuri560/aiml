661d3c64b263951f9323b6ba0138553aa29d1137
VAISHNAVI H B <vaishnavi.b@s.amity.edu>
2025-04-09 18:43:55 +0530

    using tuple

--- tuple.py
+++ tuple.py
@@ -6,3 +6,9 @@ def format_date(date):
     ]
     
     year, month, day = date
+     if 1 <= month <= 12:
+        month_name = months[month - 1]
+        print(f"{month_name} {day}, {year}")
+    else:
+        print("Invalid month value.")
+format_date(date_tuple)
