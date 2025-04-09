b391985bdb225280f5682bb45d283f1502ff35cb
VAISHNAVI H B <vaishnavi.b@s.amity.edu>
2025-04-09 18:42:17 +0530

    using tuple

---
+++ tuple.py
@@ -0,0 +1,8 @@
+date_tuple = (2025, 3, 15)
+def format_date(date):
+    months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ]
+    
+    year, month, day = date
