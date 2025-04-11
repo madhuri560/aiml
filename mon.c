69564f55e2c402e542be2db4340350a2d8a7cdc6
VAISHNAVI H B <vaishnavi.b@s.amity.edu>
2025-04-11 15:30:50 +0530

    months added

--- mon.c
+++ mon.c
@@ -4,3 +4,9 @@ int main() {
         "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
     };
+    for (int i = 0; i < 12; i++) {
+        printf("%s\n", months[i]);
+    }
+
+    return 0;
+}
