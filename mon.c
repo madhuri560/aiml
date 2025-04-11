b24c795ac796b999eb8a77edf80018b68ea9cd1d
VAISHNAVI H B <vaishnavi.b@s.amity.edu>
2025-04-11 15:29:31 +0530

    months added

---
+++ mon.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    const char *months[] = {
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    };
