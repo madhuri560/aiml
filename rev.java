a6f2cbb5d7de5db4d74c72a184d7f53e8e7aedf3
VAISHNAVI H B <vaishnavi.b@s.amity.edu>
2025-04-12 14:07:17 +0530

    using java

---
+++ rev.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int reversedNumber = 0;
