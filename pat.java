29cff96ccb6c25a49668ed48215a515407ac0bcc
VAISHNAVI H B <vaishnavi.b@s.amity.edu>
2025-04-13 22:40:14 +0530

    pattern using java

---
+++ pat.java
@@ -0,0 +1,9 @@
+public class StarPattern {
+    public static void main(String[] args) {
+        int rows = 5; // Number of rows in the pattern
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            
