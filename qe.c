08653c01fa453e45d717d5a94b5c21b47b22dc8f
VAISHNAVI H B <vaishnavi.b@s.amity.edu>
2025-05-05 10:24:59 +0530

    added new line

--- qe.c
+++ qe.c
@@ -13,4 +13,18 @@ int main() {
         printf("Roots are real and different.\n");
         printf("Root 1 = %.2lf\n", root1);
         printf("Root 2 = %.2lf\n", root2);
+    printf("Root 2 = %.2lf\n", root2);
+    } else if (discriminant == 0) {
+        root1 = root2 = -b / (2 * a);
+        printf("Roots are real and the same.\n");
+        printf("Root 1 = Root 2 = %.2lf\n", root1);
+    } else {
+        realPart = -b / (2 * a);
+        imaginaryPart = sqrt(-discriminant) / (2 * a);
+        printf("Roots are complex and different.\n");
+        printf("Root 1 = %.2lf + %.2lfi\n", realPart, imaginaryPart);
+        printf("Root 2 = %.2lf - %.2lfi\n", realPart, imaginaryPart);
+    }
+
+    return 0;
     }
