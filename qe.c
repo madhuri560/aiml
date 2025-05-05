f9e1b0cc5fb1f4546db56131cc38e6a83bded21a
VAISHNAVI H B <vaishnavi.b@s.amity.edu>
2025-05-05 09:47:13 +0530

    added new line

--- qe.c
+++ qe.c
@@ -6,4 +6,11 @@ int main() {
     printf("Enter coefficients a, b and c: ");
     scanf("%lf %lf %lf", &a, &b, &c);
     discriminant = b * b - 4 * a * c;
-}
+    if (discriminant > 0) 
+    {
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        root2 = (-b - sqrt(discriminant)) / (2 * a);
+        printf("Roots are real and different.\n");
+        printf("Root 1 = %.2lf\n", root1);
+        printf("Root 2 = %.2lf\n", root2);
+    }
