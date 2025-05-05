3133fab3bd8f9b26f94abbe267182d872649ccb0
VAISHNAVI H B <vaishnavi.b@s.amity.edu>
2025-05-05 09:38:34 +0530

    quadratic eqn

---
+++ qe.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a, b, c, discriminant, root1, root2, realPart, imaginaryPart;
+    printf("Enter coefficients a, b and c: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+    discriminant = b * b - 4 * a * c;
+}
